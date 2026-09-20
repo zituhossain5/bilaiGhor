b08f4823268130d28b27abb3fc0f9c91b6a8396e
Zitu Hossain <zituhossain5@gmail.com>
2026-09-20 19:42:04 +0600

    add fb pixel code

--- resources/views/frontEnd/layouts/master.blade.php
+++ resources/views/frontEnd/layouts/master.blade.php
@@ -571,6 +571,23 @@
         <!-- ========== End TikTok Pixel ========== -->
         @include('frontEnd.layouts.partials.ecom-tracking-lib')
         @include('frontEnd.layouts.partials.traffic-attribution')
+
+        <!-- Meta Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2646645895748988');
+            </script>
+
+            <script>
+            fbq('track', 'PageView');
+        </script>
     </head>
     <body class="gotop {{ Route::is('home') ? 'bilai-home' : '' }}">
         @foreach($gtm_code ?? [] as $gtm)
